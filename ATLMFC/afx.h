c1cd85eef712d8d48d87a472ddd73a00df62e444
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2017-08-12 14:53:26 +0200

    add headers

---
+++ ATLMFC/afx.h
@@ -0,0 +1,33 @@
+
+#ifndef __AFX_H__
+#define __AFX_H__
+
+struct __POSITION {};
+typedef __POSITION* POSITION;
+
+#define FALSE 0
+#define TRUE  1
+
+#define __deref_inout_opt
+#define __deref_in
+#define __deref_out
+#define __deref_out_opt
+#define __field_ecount_opt(X)
+#define __in
+#define __in_bcount(X)
+#define __in_ecount(X)
+#define __in_bcount_opt(X)
+#define __in_ecount_opt(X)
+#define __in_opt
+#define __inout
+#define __inout_ecount_full(X)
+#define __inout_opt
+#define __out
+#define __out_bcount(X)
+#define __out_ecount(X)
+#define __out_bcount_part(X,Y)
+#define __out_ecount_part(X,Y)
+#define __out_opt
+#define __range(X,Y)
+
+#endif
